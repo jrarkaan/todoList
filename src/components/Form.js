589c84f3a8a842319bcb03768383ebac8976745b
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 15:45:33 +0700

    menambahkan setStatus di Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Form = ({setInputText, todos, setTodos, inputText}) =>{
+const Form = ({setInputText, todos, setTodos, inputText, setStatus}) =>{
   // here i can write JS code and function
   const inputTextHandler = (e) =>{
     console.log(e.target.value);
@@ -14,6 +14,10 @@ const Form = ({setInputText, todos, setTodos, inputText}) =>{
     ]);
     setInputText('');
   }
+  const statusHandler = (e)=> {
+    //console.log(e.target.value);
+    setStatus(e.target.value);
+  }
   return(
     <form>
       <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input"/>
@@ -21,7 +25,7 @@ const Form = ({setInputText, todos, setTodos, inputText}) =>{
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
