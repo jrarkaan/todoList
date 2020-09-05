f9b04760dcf676e9f56f177e84b3885fbe66b7d9
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 15:21:39 +0700

    membereskan pengkodean

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,12 +13,13 @@ const Todo = ({text, todos, todo, setTodos})=>{
           ...item, completed: !item.completed
         }
       }
+      return item;
     }))
   }
   return(
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? 'completed' : '' }`}>{text}</li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
