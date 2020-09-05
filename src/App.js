44a7169d6070fdf2dbc040d26fd52ea024bfa591
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 08:39:39 +0700

    menambahkan const todos, setTodos = useState di app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
