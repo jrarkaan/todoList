3d11c635334c2474fae43539cf8db83c73ff20d5
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 14:10:20 +0700

    menambahkan props todo di tag todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,7 +8,7 @@
      <div className="todo-container">
       <ul className="todo-list">
         { todos.map( (todo)=> (
-          <Todo setTodos={setTodos} todos={todos} text={todo.text} key={todo.id} />
+          <Todo todo={todo} setTodos={setTodos} todos={todos} text={todo.text} key={todo.id} />
         ))}
       </ul>
     </div>
