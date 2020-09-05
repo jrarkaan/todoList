85def41dd828028dc12bed17a7d98458e128d241
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 14:03:21 +0700

    menambahkan props di todolist; parameter, dan di tag Todonya

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,13 @@
  // importing components
  import Todo from './Todo.js';
 
- const TodoList = ({todos})=>{
+ const TodoList = ({todos, setTodos})=>{
    console.log(todos);
    return(
      <div className="todo-container">
       <ul className="todo-list">
         { todos.map( (todo)=> (
-          <Todo text={todo.text} key={todo.id} />
+          <Todo setTodos={setTodos} todos={todos} text={todo.text} key={todo.id} />
         ))}
       </ul>
     </div>
