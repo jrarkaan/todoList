a9fc0d1579a45925eaf2c50c71dd20f5ecbaebc6
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 15:33:07 +0700

    menambahkan setStatus di App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import TodoList from './components/TodoList';
 function App() {
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+
   return (
     <div className="App">
       <header>
         <h1>should to do, check it {inputText}</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
       <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
