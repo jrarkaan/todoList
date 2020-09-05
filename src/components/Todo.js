3e8e19c01fde3bd61746c6a4b047219ae22f2363
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 12:33:47 +0700

    menngkoding di Todo.js

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Todo = ()=>{
+  return(
+    <div className="todo">
+      <li className="todo-item">Heyyy</li>
+      <button><i className="fas fa-check"></i></button>
+      <button><i className="fas fa-trash"></i></button>
+    </div>
+  );
+}
+
+export default Todo;
