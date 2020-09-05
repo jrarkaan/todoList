1d50846a45c04e2e402eeed1e1eb4fda02580359
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 13:45:31 +0700

    menambahkan key di todolis.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,7 +8,7 @@
      <div className="todo-container">
       <ul className="todo-list">
         { todos.map( (todo)=> (
-          <Todo text={todo.text} id={todo.id} />
+          <Todo text={todo.text} key={todo.id} />
         ))}
       </ul>
     </div>
