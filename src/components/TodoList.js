efce35944bd608c068a06a024364c55a3a4e753d
jrarkaan <rakajanitraa@hotmail.com>
2020-09-05 12:18:54 +0700

    menambahkan import Todo di dalam TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,13 @@
  import React from 'react';
+ // importing components
+ import Todo from './Todo.js';
 
  const TodoList = ()=>{
    return(
      <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        <Todo />
+      </ul>
     </div>
    );
  }
