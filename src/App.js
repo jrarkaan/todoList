21043a7c48791c0a7b3ba04d0e11d97e245a02af
jrarkaan <rakajanitraa@hotmail.com>
2020-09-06 19:23:45 +0700

    menambahkan useEffect di App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 // importing components
 import Form from './components/Form';
@@ -10,6 +10,11 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFiltered] = useState([]);
+  // useEffect
+  useEffect(()=>{
+    //console.log("hey");
+    filterHandler();
+  }, [todos, status])
   // functions
   const filterHandler = ()=>{
     switch(status){
@@ -27,7 +32,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>should to do, check it {inputText}</h1>
+        <h1>should to do, check it</h1>
       </header>
       <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
       <TodoList setTodos={setTodos} todos={todos}/>
